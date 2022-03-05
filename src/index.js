bc5e23a9267c6e8faa0f5d3815d6f552e8f4dd05
Clay Webb <clay_webb@outlook.com>
2022-03-05 15:32:59 -0600

    Created a reducer to store the selected movie from the movie list

--- src/index.js
+++ src/index.js
@@ -52,11 +52,22 @@ const genres = (state = [], action) => {
     }
 }
 
+// Selected movie reducer
+const selectedMovie = (state = [], action) => {
+    switch (action.type) {
+        case 'SELECTED_MOVIE':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
 // Create one store that all components can use
 const storeInstance = createStore(
     combineReducers({
         movies,
         genres,
+        selectedMovie
     }),
     // Add sagaMiddleware to our store
     applyMiddleware(sagaMiddleware, logger),
